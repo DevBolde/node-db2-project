633b4db78dcb5556bea93fb27783f2ac40173013
Devin Bolden <devinbolden6@gmail.com>
2023-07-22 22:35:45 -0500

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '2222222222222',
+        make: 'honda',
+        model: 'accord',
+        mileage: 233000,
+        title: 'salvage'
+    }, 
+    {
+        vin: '3333333333333',
+        make: 'hyundai',
+        model: 'elantra',
+        mileage: 200000,
+    }
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
